04741428efa3f5c684edcde9e0dbe3d68bf6475c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2022-06-05 10:03:38 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import requests
+from flask import *
+
+from json.decoder import JSONDecodeError
+
+app = Flask(__name__)
+
+JSON_Edwardpoint = "http://web.archive.org/web/20190810013707if_/https://theartistunion.com/api/v3/tracks/%s.json"
+
+@app.route("/")
+async def slash():
+    return """<form action=/Thingy method=GET><input name="id" id="id"><label for="id">Identifier (last bit of the URL, e.g. for https://theartistunion.com/tracks/900b3a it would be 900b3a)</label><br><button type="submit">Submit</button></form>"""
+
+@app.route("/JSON")
+async def get():
+    if not request.args.get("id"):
+        abort(400)
+    return requests.get(JSON_Edwardpoint % request.args["id"]).json()
+
+@app.route("/Thingy")
+async def alsoget():
+    try:
+        json = await get()
+        j = json["audio_source"], 300
+    except JSONDecodeError:
+        return "Failed to get track information. Does it exist?", 404
+    return redirect(*j)
